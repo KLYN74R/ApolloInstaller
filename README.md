a377c053628b179f87e7bda31a150685c9f6586c
Vlad Chernenko <vladchernenko@protonmail.com>
2022-10-01 19:59:03 +0300

    Added fetch docker version and open browser

---
+++ README.md
@@ -0,0 +1,9 @@
+# <b>Apollo installer</b>
+
+A cross-platform executable to run Apollo automatically, prepare containers and so on
+
+<br/>
+
+## <b>More info</b>
+
+Coming soon
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::process::Command;
+use webbrowser::open;
+
 fn main() {
-    println!("Hello, world!");
+
+    open("http://github.com/KLYN74R");
+
+    //Let's also check docker version on machine
+    let docker_version = String::from_utf8(Command::new("docker").arg("--version").output().unwrap().stdout).unwrap();
+
+    println!("Docker version: {}",docker_version);
+
 }
